f5b55bafad84fd20997cb69362d6b38fe568c639
braghirolife <febraghiroli@hotmail.com>
2023-08-13 14:15:54 -0300

    .

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import express, {Request, Response, NextFunction} from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import helmet from 'helmet';
+import myDataSource from "./infra/app-data-source.ts"
+
+myDataSource
+    .initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization:", err)
+    })
+
+const app = express()
+
+app.use(morgan('tiny'));
+
+app.use(cors());
+
+app.use(helmet());
+
+app.use(express.json());
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.send(`INCOMING REQUEST ${req.method}`);
+})
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+    res.status(500).send(error.message);
+})
+
+export default app;
